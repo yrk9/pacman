53b9bd2447ade029113d8cef19511329eb5a29cd
keiFukuda <kei08.fukuda@gmail.com>
2024-11-05 17:04:56 +0900

    game.py更新

--- game.py
+++ game.py
@@ -2,12 +2,13 @@ import time
 from player import Player
 from field import Field
 from config import Parameters
+from user_input import UserInput
 import os
 
 
 class Game:
     """Gameクラス
-    
+
     Attributes:
         players (list[Player]): プレイヤー
         field (Field): フィールド
@@ -15,7 +16,7 @@ class Game:
 
     def __init__(self, params: Parameters) -> None:
         """初期化
-        
+
         Args:
             params (Parameters): configのパラメータのインスタンス
         """
@@ -31,7 +32,7 @@ class Game:
             param (Parameters): configのパラメータのインスタンス
         """
         f_size = params.field_size
-        #フィールドの初期化
+        # フィールドの初期化
         self.players = [Player(1, 1, "😊")]
         self.field = Field(self.players, f_size)
 
@@ -39,24 +40,23 @@ class Game:
         """ゲームのメインループ
 
         Returns:
-            str: ゲーム終了時のメッセージ 
+            str: ゲーム終了時のメッセージ
         """
-        #ゲームのメインループ
+        # ゲームのメインループ
         while True:
             os.system("cls" if os.name == "nt" else "clear")
             self.field.print_field()
 
-            #プレイヤーの移動を決定
+            # プレイヤーの移動を決定
             for player in self.players:
                 # キー入力を受け取る
                 key = UserInput.get_user_input()
                 player.get_next_pos(key)
 
-            #プレイヤーの移動
+            # プレイヤーの移動
             for item in self.players:
                 item.update_pos(stuck=True)
 
             self.field.update_field()
 
             time.sleep(0.3)
-
