301b3ecfb2d00a124ed0411a597d9a49c5367613
yrs243151 <s243151@stn.nagaokaut.ac.jp>
2024-11-05 16:51:09 +0900

    game.py中身を追加

--- game.py
+++ game.py
@@ -19,7 +19,10 @@ class Game:
         Args:
             params (Parameters): configのパラメータのインスタンス
         """
-        pass
+        self.players = []
+        self.field = None
+        self.setup(params)
+        self.start()
 
     def setup(self, params: Parameters) -> None:
         """初期設定を行う
@@ -27,6 +30,10 @@ class Game:
         Args:
             param (Parameters): configのパラメータのインスタンス
         """
+        f_size = params.field_size
+        #フィールドの初期化
+        self.players = [Player(1, 1, "😊")]
+        self.field = Field(self.players, f_size)
 
     def start(self) -> None:
         """ゲームのメインループ
@@ -34,5 +41,22 @@ class Game:
         Returns:
             str: ゲーム終了時のメッセージ 
         """
-        pass
+        #ゲームのメインループ
+        while True:
+            os.system("cls" if os.name == "nt" else "clear")
+            self.field.print_field()
+
+            #プレイヤーの移動を決定
+            for player in self.players:
+                # キー入力を受け取る
+                key = UserInput.get_user_input()
+                player.get_next_pos(key)
+
+            #プレイヤーの移動
+            for item in self.players:
+                item.update_pos(stuck=True)
+
+            self.field.update_field()
+
+            time.sleep(0.3)
 
